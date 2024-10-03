a2e86b801916b79ce3f9d1ea76995b255bd05cb4
Alain Hélaïli <alain@carbonhub.io>
2024-10-03 16:44:30 +0200

    device code retrieved

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 # Entry point of the application
 import os
-
 import requests
 
 # Retrieve the GitHub App ID and private key from the environment
 client_id = os.getenv("GITHUB_CLIENT_ID")
-private_key = os.getenv("GITHUB_APP_PRIVATE_KEY")
+private_key_file = os.getenv("GITHUB_APP_PRIVATE_KEY_FILE")
 
 # Send a POST request to https://github.com/login/device/code along with a client_id query parameter
 # The response will contain a user_code, device_code, and verification_uri
@@ -13,8 +12,13 @@ url = "https://github.com/login/device/code"
 params = {
     "client_id": client_id
 }
-response = requests.post(url, params=params)
+headers = {
+    "Accept": "application/json"
+}
+response = requests.post(url, params=params, headers=headers)
 response.raise_for_status()
+
+print(f"Device code request status: {response.status_code}")
 
 data = response.json()
 device_code = data["device_code"]
